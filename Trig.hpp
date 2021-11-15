51eec8af186eb5f9eac15bfc91dec200141adeb9
SpudY2K <87917486+SpudY2K@users.noreply.github.com>
2021-11-15 02:18:47 +0000

    Store entire arctan table
    Functionally the same and makes things easier to parallelise.

--- Trig.hpp
+++ Trig.hpp
@@ -7,7 +7,7 @@
 
 extern float gSineTable[4096];
 extern float gCosineTable[4096];
-extern int gArctanTable[1025];
+extern int gArctanTable[8192];
 
 int16_t atan2_lookup(float z, float x);
 int16_t atan2s(float z, float x);
